cd993d4829612713ee49b86b6f977f83aaa81c25
petr_shirin <p.shirin@agora.ru>
2021-12-17 20:49:40 +0800

    Простой проект с классом вектора

---
+++ src/main.lua
@@ -0,0 +1,19 @@
+require("vector")
+
+
+function love.load()
+    width = love.graphics.getWidth()
+    height = love.graphics.getHeight()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+end
+
+function love.mousereleased(x, y, button, istouch,  presses)
+end    
